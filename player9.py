8b791096d2932be297fb872d22b67a285db8b688
aash19i <43963884+aash19i@users.noreply.github.com>
2018-11-16 23:26:26 +0530

    Add files via upload

--- player9.py
+++ player9.py
@@ -2,10 +2,12 @@ print("enter the lower limit")
 l=int(input())
 print("enter higher limit")
 h=int(input())
+c=0
 for i in range(l,h+1):
     if i>1:
         for j in range(2,i):
             if i%j==0:
                 break
         else:
-            print(i)
+            c+=1
+print(c)
