c7711de244e0c0e8ab2b0db4fd64c374dcd2b4c8
aash19i <43963884+aash19i@users.noreply.github.com>
2019-01-19 23:02:53 +0530

    Add files via upload

---
+++ Spaces.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+class Spaces
+{
+	public static void main(String[] args) 
+	{
+		String a;
+		int c=0;
+		Scanner s=new Scanner(System.in);
+		a=s.nextline();
+		for(int i=o;i<a.length;i++)
+			if (a.charAt(i)==' ')
+			{c++;
+			}
+		System.out.println(c);
+	}
+}
