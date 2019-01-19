105d8d4ba77baac774ecf35917c0f5ef962a56e7
aash19i <43963884+aash19i@users.noreply.github.com>
2019-01-19 22:47:32 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+class A 
+{
+	public static void main(String[] args) 
+	{
+		String a;
+		int count=0;
+		Scanner s=new Scanner(System.in);
+		a=s.nextline();
+		for(int i=0;i<a.length;i++){
+			if(a.charAt(i)!=' '){
+				count+;}}
+		System.out.println(count);
+	}
+}
